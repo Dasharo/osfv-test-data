e7957e3d8188ea6c7e54fb7dd7ec0b83a3e74826
Wiktor Mowinski <wiktor.mowinski@3mdeb.com>
2025-11-06 14:47:23 +0100

    setup.sh: add file download in setup
    Signed-off-by: Wiktor Mowinski <wiktor.mowinski@3mdeb.com>

--- setup.sh
+++ setup.sh
@@ -10,5 +10,22 @@ if pushd dts; then
   popd
 fi
 
-git annex unlock dasharo-driver/dasharo-acpi-dkms-0.9.1*
+#!/usr/bin/env bash
+set -e
 
+# Post-clone setup script
+
+echo "Preparing Dasharo driver packages in annex..."
+
+git annex addurl --fast \
+  --file=dasharo-driver/dasharo-acpi-dkms-0.9.1.x86_64.rpm \
+  https://github.com/Dasharo/dasharo-acpi-dkms/releases/download/v0.9.1/dasharo-acpi-dkms-0.9.1.x86_64.rpm
+
+git annex addurl --fast \
+  --file=dasharo-driver/dasharo-acpi-dkms-0.9.1_amd64.deb \
+  https://github.com/Dasharo/dasharo-acpi-dkms/releases/download/v0.9.1/dasharo-acpi-dkms_0.9.1_amd64.deb
+
+
+# Make sure annex content is present
+echo "Fetching annex content..."
+git annex get dasharo-driver/dasharo-acpi-dkms-0.9.1*
