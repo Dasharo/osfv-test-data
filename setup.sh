dd1e2c3f4139313052a8cd601a26cc697340628c
Wiktor Mowinski <wiktor.mowinski@3mdeb.com>
2025-11-06 15:00:02 +0100

    setup.sh: revert superfluous changes
    Signed-off-by: Wiktor Mowinski <wiktor.mowinski@3mdeb.com>

--- setup.sh
+++ setup.sh
@@ -10,22 +10,3 @@ if pushd dts; then
   popd
 fi
 
-#!/usr/bin/env bash
-set -e
-
-# Post-clone setup script
-
-echo "Preparing Dasharo driver packages in annex..."
-
-git annex addurl --fast \
-  --file=dasharo-driver/dasharo-acpi-dkms-0.9.1.x86_64.rpm \
-  https://github.com/Dasharo/dasharo-acpi-dkms/releases/download/v0.9.1/dasharo-acpi-dkms-0.9.1.x86_64.rpm
-
-git annex addurl --fast \
-  --file=dasharo-driver/dasharo-acpi-dkms-0.9.1_amd64.deb \
-  https://github.com/Dasharo/dasharo-acpi-dkms/releases/download/v0.9.1/dasharo-acpi-dkms_0.9.1_amd64.deb
-
-
-# Make sure annex content is present
-echo "Fetching annex content..."
-git annex get dasharo-driver/dasharo-acpi-dkms-0.9.1*
