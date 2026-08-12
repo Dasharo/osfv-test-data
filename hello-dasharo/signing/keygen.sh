acce9b9387cd125a50a2f0b41ac8293f1a21ae03
Oktawian Bieszke <oktawian.bieszke@3mdeb.com>
2026-08-12 15:46:00 +0200

    osfv-test-data/hello-dasharo: add expired and root-ca certificate/image generation
    Signed-off-by: Oktawian Bieszke <oktawian.bieszke@3mdeb.com>

--- hello-dasharo/signing/keygen.sh
+++ hello-dasharo/signing/keygen.sh
@@ -2,6 +2,8 @@
 
 CERT_EXPIRATION_DAYS=730
 CERT_SUBJECT="/C=pl/ST=pomorskie/L=Gdańsk/O=3mdeb/OU=dasharo-team/CN=BAD_INFLUE"
+LEAF_SUBJECT="/C=pl/ST=pomorskie/L=Gdańsk/O=3mdeb/OU=dasharo-team/CN=BAD_INFLUE Leaf"
+CA_SUBJECT="/C=pl/ST=pomorskie/L=Gdańsk/O=3mdeb/OU=dasharo-team/CN=BAD_INFLUE Root CA"
 
 echo "Keys & certificates generation ..."
 
@@ -10,6 +12,8 @@ rm -f *.der
 
 openssl genrsa -out private-key-good.pem 3027
 openssl genrsa -out private-key-bad.pem 3027
+openssl genrsa -out private-key-expired.pem 3027
+openssl genrsa -out private-key-root-ca.pem 3027
 
 openssl rsa -in private-key-good.pem -pubout -out public-key-good.pem
 openssl rsa -in private-key-bad.pem -pubout -out public-key-bad.pem
@@ -21,5 +25,22 @@ openssl req -new -x509 -key private-key-bad.pem -out cert_bad.pem \
 
 openssl x509 -outform der -in cert_good.pem -out cert_good.der
 
-echo "... Done."
+openssl req -new -x509 -key private-key-root-ca.pem -out cert_root_ca.pem \
+	-days $CERT_EXPIRATION_DAYS -nodes -subj "$CA_SUBJECT"
 
+openssl x509 -outform der -in cert_root_ca.pem -out cert_root_ca.der
+
+openssl req -new -newkey rsa:3072 -nodes \
+	-keyout private-key-leaf.pem -out req_leaf.csr \
+	-subj "$LEAF_SUBJECT"
+
+openssl x509 -req -in req_leaf.csr \
+	-CA cert_root_ca.pem -CAkey private-key-root-ca.pem -CAcreateserial \
+	-out cert_leaf.pem -days $CERT_EXPIRATION_DAYS
+cat cert_leaf.pem cert_root_ca.pem >cert_chain.pem
+
+faketime '1970-01-01 19:41:00' openssl req -new -x509 -key private-key-expired.pem -out cert_expired.pem \
+	-days $CERT_EXPIRATION_DAYS -nodes -subj "$CERT_SUBJECT"
+openssl x509 -outform der -in cert_expired.pem -out cert_expired.der
+
+echo "... Done."
